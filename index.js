e4d8b176d3a1a2284c65ec594eb8ce2f814f2144
Leen Zien <Loli@Mac.fritz.box>
2025-02-03 21:56:22 +0100

    solve the lab-intro-node by leen

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
 
-  get(pos) {}
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
 
-  max() {}
+  }
 
-  min() {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutOfBounds");
+    } else
+      return this.items[pos];
+  }
 
-  sum() {}
+  max() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }else
+    return Math.max(...this.items);
+    
+   }
 
-  avg() {}
+  min() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }else
+    return Math.min(...this.items);
+    
+   }
+
+  sum() {
+    let res =0;
+    if(this.length === 0){
+      return 0;
+    }
+   for(let i=0; i<this.length; i++){
+    res += this.items[i];
+   }
+    return res;
+   }
+
+  avg() { 
+    let res =0;
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+   for(let i=0; i<this.length; i++){
+    res += this.items[i];
+   }
+   return res/this.length;
+  }
 }
 
 module.exports = SortedList;
+
+const list = new SortedList();
+console.log(list.items);
+console.log(list.length);
